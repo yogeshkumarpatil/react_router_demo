556062a72aa209acf73ca1adc7fe8344cbcb5f7e
yogeshkumarpatil <appseeds.yogeshkumar@gmail.com>
2023-04-30 16:38:07 +0530

    progarammatically nagivating complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
+  const navigate = useNavigate();
+
+  //he single route la call karnyasathi
+  // const NavigateTOAbout = () =>{
+  //   navigate('/about');
+  // }
+
+
+  //he multiple route la call karnyasathi
+  // const NavigateTo = (url) =>{
+  //   navigate(url);
+  // }
+
+
+
+  const NavigateToWhere = () =>{
+    let name = 'Yogessh';
+    if(name === 'Yogesh')
+    {
+      navigate('/about');
+    }
+    else{
+      navigate('contact')
+    }
+  }
+
+
+
   return (
     <>
     <Navbar />
@@ -15,6 +43,27 @@ function App() {
       <Route path='/about' element={ <About />}  />
       <Route path='/contact' element={ <Contact />}  />
     </Routes>
+
+    {/* <button onClick={ () => NavigateTOAbout()}>About 1 </button> */}
+
+
+
+    {/* on button click navigation */}
+    {/* <button onClick={ () => navigate('/about')}>About 2</button>     */}
+    {/* <button onClick={ () => navigate('/contact')}>Contact</button> */}
+
+
+
+    {/* on event handler navigation */}
+    {/* <button onClick={ () => NavigateTo('/about')}>About 2</button>
+    <button onClick={ () => NavigateTo('/contact')}>Contact</button> */}
+
+
+    {/* on condition based navigation */}
+    <button onClick={ () => NavigateToWhere()}>Click Me</button>
+
+    <button onClick={ () => navigate(-1)}>Go Back</button>
+
     </>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 function Contact() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Welcome To my Contact Page yogesh 3</h1>
+      
     </div>
   )
 }
