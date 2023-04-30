8b7ca06adbd7a6b6111c57da1eb2a7d2c1806141
yogeshkumarpatil <appseeds.yogeshkumar@gmail.com>
2023-04-30 13:50:13 +0530

    navbar complete

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path='/' element={ <Home />}  />
       <Route path='/about' element={ <About />}  />
       <Route path='/contact' element={ <Contact />}  />
     </Routes>
+    </>
   );
 }
 
