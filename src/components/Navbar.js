33042b0d814b074a2429944855c5a4edd9b26fae
yogeshkumarpatil <appseeds.yogeshkumar@gmail.com>
2023-04-30 14:35:37 +0530

    isActive is complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+
 
 function Navbar() {
+   const NavLinkCSS = ({isActive}) =>{
+    return{
+        fontWeight: isActive ? 'bold' : 'normal',
+        fontSize: isActive ? '23px' : '20px',
+    }
+   }
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
+        <NavLink style={NavLinkCSS} to='/'>Home</NavLink>
+        <NavLink style={NavLinkCSS} to='/about'>About</NavLink>
+        <NavLink style={NavLinkCSS} to='/contact'>Contact</NavLink>
     </nav>
   )
 }
